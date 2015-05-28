babd85b162834c9bd3b63a298baefe39848bf68f
smallTigerBrother <664674103@qq.com>
2015-05-28 19:26:42 +0800

    修改类路径

--- ButterKnifeForTiger/src/butterknife/ButterKnifeExtendByTiger.java
+++ ButterKnifeForTiger/src/butterknife/ButterKnifeExtendByTiger.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.view.View;
 import android.view.ViewGroup;
-import butterknife.ButterKnife.ViewBinder;
 
 public class ButterKnifeExtendByTiger
 {
